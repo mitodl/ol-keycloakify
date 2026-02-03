db234eafa4b586deed424cabc3f5ff0b297e085e
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2026-02-03 17:54:53 +0100

    Remove logs

--- src/login/utils/isOrgEmail.ts
+++ src/login/utils/isOrgEmail.ts
@@ -5,8 +5,6 @@ export function isOrgEmail(email?: string): boolean {
   const emailParts = email.trim().split("@")
   if (emailParts.length !== 2) return false
   const domain = emailParts[1].toLowerCase()
-  console.log(domain)
-  console.log(ORG_EMAIL_DOMAINS)
   return ORG_EMAIL_DOMAINS.some(
     (orgEmailDomain: string) =>
       domain === orgEmailDomain.toLowerCase() ||
